09665fc3a37d52f866e472d834cb051e010a4e0a
Mikhail Zolenko <mihail.zolenko@gmail.com>
2021-04-12 01:42:01 +0600

    added house page

---
+++ pages/house/[id].js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import ClickContext from '../../contexts/click';
+
+const HousePage = () => {
+  const [clickHouse] = useContext(ClickContext);
+
+  return (
+    <div></div>
+  )
+}
+
+export default HousePage;
