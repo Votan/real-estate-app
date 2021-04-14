0ac755cf31f4562b58a0c921a679c65f7209a9b0
Mikhail Zolenko <mihail.zolenko@gmail.com>
2021-04-14 18:05:31 +0600

    fixed indent

--- components/HouseDetailed.js
+++ components/HouseDetailed.js
@@ -4,37 +4,37 @@ import { useContext } from 'react';
 import ClickContext from '../contexts/click';
 
 const Image = styled.img`
-    display: inline-block;
-    height: 300px;
-    margin: -30px;
-    position: relative;
-    width: calc(100% + 60px);
+  display: inline-block;
+  height: 300px;
+  margin: -30px;
+  position: relative;
+  width: calc(100% + 60px);
 `
 
 const Title = styled.h2`
-    padding: 0;
+  padding: 0;
 `
 
 const Description = styled.div`
-    color: blue
+  color: blue
 `
 
 const Price = styled.div`
-    color: green;
-    font-weight: bold;
+  color: green;
+  font-weight: bold;
 `
 
 const HouseDetailed = () => {
-    const [clickHouse] = useContext(ClickContext);
+  const [clickHouse] = useContext(ClickContext);
 
-    return (
-        <div>
-            <Image src={clickHouse.images[0]}></Image>
-            <Title>{clickHouse.product}</Title>
-            <Description>{clickHouse.description}</Description>
-            <Price>$ {clickHouse.price}</Price>
-        </div>
-    )
+  return (
+    <div>
+      <Image src={clickHouse.images[0]}></Image>
+      <Title>{clickHouse.product}</Title>
+      <Description>{clickHouse.description}</Description>
+      <Price>$ {clickHouse.price}</Price>
+    </div>
+  )
 }
 
 export default HouseDetailed;
